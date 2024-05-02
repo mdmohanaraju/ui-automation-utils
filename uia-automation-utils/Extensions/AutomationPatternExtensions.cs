697913e0c0f2d0936e6ad5ebbe30b475eb90fe83
mdmohanaraju <mdmohanaraju@gmail.com>
2024-05-03 02:57:29 +0530

    Add code for RangeValue pattern

--- uia-automation-utils/Extensions/AutomationPatternExtensions.cs
+++ uia-automation-utils/Extensions/AutomationPatternExtensions.cs
@@ -1,11 +1,27 @@
 ﻿using System;
+using System.Windows.Automation;
+
 namespace UIA.Automation.Utils.Extensions
 {
-	public class AutomationPatternExtensions
+	public static class AutomationPatternExtensions
 	{
-		public AutomationPatternExtensions()
+		public static T GetPattern<T>(this AutomationElement element, AutomationPattern pattern)
 		{
-		}
-	}
+			var isSupportedPattern = element.TryGetCurrentPattern(pattern, out var patternObj);
+			if (isSupportedPattern)
+			{
+				return (T)patternObj;
+			}
+
+#pragma warning disable CS8603 // Possible null reference return.
+            return default;
+#pragma warning restore CS8603 // Possible null reference return.
+        }
+
+        public static InvokePattern GetInvokePattern(this AutomationElement element)
+        {
+			return element.GetPattern<InvokePattern>(InvokePattern.Pattern);			
+        }
+    }
 }
 
---
+++ uia-automation-utils/Extensions/RangeValue.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows.Automation;
+
+namespace UIA.Automation.Utils.Extensions
+{
+	public static class AutomationElementExtensions
+	{
+        #region Set range for a slider
+
+        public static void SetRange(this AutomationElement element, double value)
+		{
+            var rangeValuePattern = element.GetPattern<RangeValuePattern>(RangeValuePattern.Pattern);
+            rangeValuePattern?.SetValue(value);
+        }
+
+        public static bool IsReadOnly(this AutomationElement element)
+        {
+            var rangeValuePattern = element.GetPattern<RangeValuePattern>(RangeValuePattern.Pattern);
+            return rangeValuePattern.Current.IsReadOnly;
+        }
+
+        public static double GetRange(this AutomationElement element)
+        {
+            var rangeValuePattern = element.GetPattern<RangeValuePattern>(RangeValuePattern.Pattern);
+            return rangeValuePattern.Current.Value;
+        }
+
+        public static double GetMaximumRange(this AutomationElement element)
+        {
+            var rangeValuePattern = element.GetPattern<RangeValuePattern>(RangeValuePattern.Pattern);
+            return rangeValuePattern.Current.Maximum;
+        }
+
+        public static double GetMinimumRange(this AutomationElement element)
+        {
+            var rangeValuePattern = element.GetPattern<RangeValuePattern>(RangeValuePattern.Pattern);
+            return rangeValuePattern.Current.Minimum;
+        }
+
+        #endregion
+    }
+}
+
