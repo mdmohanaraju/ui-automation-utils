4bbc35c342615964cc4d96a7cf896daca3e5d0d9
mdmohanaraju <mdmohanaraju@gmail.com>
2024-09-22 22:29:35 +0530

    Updated author

--- uia-automation-utils/Extensions/Extensions.TextPattern.cs
+++ uia-automation-utils/Extensions/Extensions.TextPattern.cs
@@ -1,4 +1,4 @@
-﻿#region Copyright 2024 MohanrajDevadoss
+﻿#region Copyright 2024 mdmohanaraju
 
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
